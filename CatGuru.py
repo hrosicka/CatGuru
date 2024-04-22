b7c8c091acfaeb21d896d0b947454fd1c24c1059
Hanka <hrosicka@post.cz>
2024-04-22 17:13:57 +0200

    Design improvements

--- CatGuru.py
+++ CatGuru.py
@@ -1,7 +1,9 @@
 import os
 from ctypes import windll
 import tkinter as tk
+import customtkinter
 from PIL import Image, ImageTk
+from idlelib.tooltip import Hovertip
 import requests
 
 # Enhance DPI awareness for better scaling on high-DPI displays
@@ -27,8 +29,8 @@ AVATARS = [
 # Background color options for the app
 BACKGROUND_COLORS = [
   "lightblue",  # Light blue
-  "lightgreen",  # Light green
-  "lightyellow", # Light yellow
+  "lightgrey",  # Light grey
+  "lightgoldenrod", # Light goldenrod
   "pink", # Pink
 ]
 
@@ -37,7 +39,7 @@ class CatGuru:
         # Create the main window
         self.window = tk.Tk()
         self.window.title("My Cat Guru")
-        self.window.geometry("480x480")
+        self.window.geometry("690x550")
 
         ico = Image.open(os.path.join(dirname, 'icon.jpg'))
         photo = ImageTk.PhotoImage(ico)
@@ -52,8 +54,9 @@ class CatGuru:
         # Set initial cat avatar
         self.avatar_index = 0
         self.avatar_image = self.get_image(AVATARS[self.avatar_index])
-        self.avatar_label = tk.Label(self.window, image=self.avatar_image)
-        self.avatar_label.pack(padx=10, pady=10)
+        self.avatar_label = tk.Label(self.window, image=self.avatar_image)                 
+
+        self.avatar_label.pack(padx=20, pady=20)
 
         # Create options panel at the bottom
         self.options_frame = tk.Frame(self.window)
@@ -61,22 +64,50 @@ class CatGuru:
 
         # Label to display the cat wisdom
         self.wisdom_label = tk.Label(self.window, text="", font=("Arial", 12))
+        self.wisdom_label.config(bg=self.background_color)    
         self.wisdom_label.pack(side=tk.BOTTOM, fill=tk.X)
 
         # Button to generate wisdom
-        self.wisdom_button = tk.Button(self.options_frame, text="Ask the Cat Guru", command=self.show_wisdom,
-                                       bg="snow2")
-        self.wisdom_button.pack(side=tk.LEFT, padx=5, pady=5)
+        self.wisdom_button = customtkinter.CTkButton(master=self.options_frame,
+                                                    text="Ask the Cat Guru",
+                                                    command=self.show_wisdom,
+                                                    corner_radius=15,
+                                                    text_color="snow2",
+                                                    fg_color="#393D3F",
+                                                    hover_color="#271F30") 
+        self.wisdom_button.pack(side=tk.LEFT, padx=5, pady=10)
 
         # Button to change avatar
-        self.avatar_button = tk.Button(self.options_frame, text="Change Avatar", command=self.change_avatar,
-                                       bg="snow2")
-        self.avatar_button.pack(side=tk.LEFT, padx=5, pady=5)
+        self.avatar_button = customtkinter.CTkButton(master=self.options_frame,
+                                                    text="Change Avatar",
+                                                    command=self.change_avatar,
+                                                    corner_radius=15,
+                                                    text_color="snow2",
+                                                    fg_color="#393D3F",
+                                                    hover_color="#271F30") 
+        
+        self.avatar_button.pack(side=tk.LEFT, padx=5, pady=10)
 
         # Button to change background
-        self.background_button = tk.Button(self.options_frame, text="Change Background", command=self.change_background, 
-                                           bg="snow2")
-        self.background_button.pack(side=tk.LEFT, padx=5, pady=5)
+        self.background_button = customtkinter.CTkButton(master=self.options_frame,
+                                                        text="Change Background",
+                                                        command=self.change_background,
+                                                        corner_radius=15,
+                                                        text_color="snow2",
+                                                        fg_color="#393D3F",
+                                                        hover_color="#271F30") 
+
+        self.background_button.pack(side=tk.LEFT, padx=5, pady=10)
+
+        # Tooltips
+        self.wisdom_age_button_tooltip = "Need some purrfect advice? Click here to unleash the wisdom of the Cat Guru!"
+        self.avatar_button_tooltip = "Feeling like a different cat today? Switch up your avatar and show off your unique feline flair."
+        self.background_button_tooltip = "Bored of your old background? Change it to a color that's meow-gical!"
+
+        # Set tooltips
+        Hovertip(self.wisdom_button, self.wisdom_age_button_tooltip)
+        Hovertip(self.avatar_button, self.avatar_button_tooltip)
+        Hovertip(self.background_button, self.background_button_tooltip)
 
 
     def show_wisdom(self):
@@ -86,7 +117,7 @@ class CatGuru:
 
         # Split the text into multiple lines for better readability
         wisdom_text = data["fact"]
-        max_length = 35  # Maximum characters per line
+        max_length = 45  # Maximum characters per line
         lines = []
         for word in wisdom_text.split():
             if lines:  # Check if lines is not empty
@@ -114,6 +145,7 @@ class CatGuru:
 
         # Update background label color
         self.background_label.config(bg=self.background_color)
+        self.wisdom_label.config(bg=self.background_color)
 
     def get_image(self, filename):
         # Load image and resize it for GUI
